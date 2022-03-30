44ab5e4c8ba634f52823ad913242d7f00729889c
IndreBalt <90377920+IndreBalt@users.noreply.github.com>
2022-03-30 18:41:01 +0300

    New

---
+++ ClockDegrees/ClockDegrees/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace ClockDegrees
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {           
+
+            while (true)
+            {
+                int hours;
+                int writtenHours;
+                int minutes;
+
+                while (true)
+                {
+                    Console.WriteLine("Enetr analogue hours (0-12): ");
+                    hours = int.Parse(Console.ReadLine());
+                    writtenHours = hours;
+                    if (hours < 0 || hours > 12)
+                    {
+                        Console.WriteLine("Wrong hours, it must be btween 0 and 12");
+                    }
+                    else
+                    {
+                        if (hours == 12)
+                        {
+                            hours = 0;
+                            break;                            
+                        }
+                        break;
+                    }
+                }
+
+                while (true)
+                {
+                    Console.WriteLine("Enter minutes (0-59): ");
+                    minutes = int.Parse(Console.ReadLine());
+                    if(minutes < 0 || minutes >= 60)
+                    {
+                        Console.WriteLine("Wrong minutes, it must be btween 0 and 59");
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }                
+
+                Console.WriteLine($"time:{writtenHours}:{minutes}");
+
+                int hoursDegrees = (hours * 30) + (minutes * 1 / 2);
+                int minutesDegrees = (minutes * 6);
+                Console.WriteLine();
+                int degrees;
+                if (minutesDegrees > hoursDegrees)
+                {
+                    degrees = minutesDegrees - hoursDegrees;
+                }
+                else
+                {
+                    degrees = hoursDegrees - minutesDegrees;
+                }
+                if (degrees > 180)
+                {
+                    degrees = 360 - degrees;
+                }
+                Console.WriteLine($"Lesser angle between arrows is: {degrees}");
+
+                Console.WriteLine("Dou you want to repeat? (yes/no)");
+                if(Console.ReadLine().ToLower() != "yes")
+                {
+                    break;
+                }  
+            }   
+        }
+    }
+}
---
+++ Herarchy/Herarchy/Branch.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herarchy
+{
+    public class Branch
+    {
+        List<Branch> branches;
+
+    }
+
+
+}
